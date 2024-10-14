068c6b138066282e8634e9f6b145bbe86541ea03
nikhil-fd <150642109+nikhil-fd@users.noreply.github.com>
2024-10-14 19:33:07 +0000

    explained distributed computing

---
+++ 10_distributed_computing/i_distributed_computing.js
@@ -0,0 +1,8 @@
+//🚩Distributed Computing System:🎁
+//Distributed computing system allows communication between two application these application
+//running in two different system/machine.
+
+//V.V.I
+//Actually real time job most of the company gives you API and that API stored all data
+//such as image, table etc. these data you have to fetch from API and accordingly we
+//have to use these data in our frontend UI.
